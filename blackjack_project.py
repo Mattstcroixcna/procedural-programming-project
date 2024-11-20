aea67a6311a45263a61c3539781804c1c7f29053
matt stc <matthew.st.croix21@ed.cna.nl.ca>
2024-11-20 19:15:39 -0330

    added function to make the deck

--- blackjack_project.py
+++ blackjack_project.py
@@ -16,5 +16,17 @@ def bet():
         cash = int(file.read())
         print(cash)
 
-if __name__ == "__main__":
-    main()
+def make_cards():
+    suits = ['Hearts', 'Diamonds', 'CLubs', 'Spades']
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    rank_value = {
+        '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, 
+    '7': 7, '8': 8, '9': 9, '10': 10, 
+    'J': 10, 'Q': 10, 'K': 10, 'A': [1, 11]
+    }
+    deck = []
+
+    for suit in suits:
+        for rank in ranks:
+            card = {'card': f"{rank} of {suit}", 'value': rank_value[rank]}
+            deck.append(card)
