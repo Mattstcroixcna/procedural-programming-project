004a64d376d67a71ea29313ae1c494faaf1afcb2
matt stc <matthew.st.croix21@ed.cna.nl.ca>
2024-11-26 16:37:44 -0330

    final version of the project

--- blackjack_project.py
+++ blackjack_project.py
@@ -2,9 +2,13 @@ import random
 # this block of code is where im storing global variables
 deck = []
 dealer = []
-dealer_card_only = []
 player = []
 draw = 1
+play = 1
+player_points = 0
+dealer_points = 0
+with open('money.txt', 'r') as file:
+        cash = int(file.read())
 
 
 
@@ -13,11 +17,24 @@ def main():
     print("Blackjack")
     print("blackjack payment is 3:2")
     # at first i had the game play through main but i made game its own function so i could repeat stuff
-    play = 1
     while play == 1:
         game()
 
 def game():
+    global draw
+    global cash
+    global play
+
+    # Reset deck and hands
+    reset_deck()
+    player.clear()
+    dealer.clear()
+    
+    cash = int(cash)
+    if cash < 5:
+        purchase()
+    else:
+        pass
     bet()
     draw_dealer_card()
     draw_dealer_card()
@@ -27,19 +44,52 @@ def game():
     draw_player_card()
     print("YOUR HAND:")
     show_player_cards()
+    player_total()
+    dealer_total()
 
-    # while draw is equal to 1 the player can hit or stand
+    # Player turn
+    draw = 1
     while draw == 1:
         hit_or_stand()
-        player_points = player_total()
+        player_total()
+        dealer_total()
         check_player_bust(player_points)
-        if draw == 2:
+        if draw == 2 and player_points <= 21:
             print("Game Over. You chose to stand!")
-            break  # Exit the loop if player stands
+            print("DEALERS CARDS:")
+            show_dealer_cards()
+            print()
+            print("YOUR POINTS: ", player_points)
+            print("DEALERS POINTS: ", dealer_points)
+            check_win(player_points, dealer_points)
+            break
         elif player_points > 21:
             print("You busted!")
-            break  # Exit the loop if the player busts
+            break
     
+    print()
+    choice = input("would you like to continue (y/n):")
+    if choice == 'y':
+        play = 1
+    elif choice == 'n':
+        play = 2
+        return play
+
+
+    
+def purchase():
+    global cash
+    print("BUY SOME TOKENS!!")
+    print("100 tokens - 10 dollars")
+    print("200 tokens - 20 dollars")
+    print("500 tokens - 40 dollars")
+    choice = int(input("how many tokens would you like (100, 200, 500): "))
+    with open('money.txt', 'r') as file:
+        cash = cash + choice
+        cash = str(cash)
+    with open('money.txt', 'w') as file:
+        file.write(cash)
+        
 
 
     
@@ -61,17 +111,28 @@ def bet():
         print(cash)
         return bet_amount
 
+#makes the deck
 def make_cards():
-    suits = ['Hearts', 'Diamonds', 'CLubs', 'Spades']
+    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
     ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     rank_value = {
-        '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,'J': 10, 'Q': 10, 'K': 10, 'A': [1, 11]
+        '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+        'J': 10, 'Q': 10, 'K': 10, 'A': [1, 11]
     }
 
+    global deck
     for suit in suits:
         for rank in ranks:
             card = {'card': f"{rank} of {suit}", 'value': rank_value[rank]}
             deck.append(card)
+
+
+#resets the deck before each game
+def reset_deck():
+    global deck
+    deck = []
+    make_cards()
+
 
 # the next two functions handle drawing cards for the dealer and player respectively using the random module
 def draw_dealer_card():
@@ -84,7 +145,7 @@ def draw_player_card():
     card = random.choice(deck)
     player.append(card)
     deck.remove(card)
-    handle_ace_choice(card)
+    handle_ace_choice(card, player_points)
     return card['card']
 
 # the next two functions show the cards as "rank of suit" for the dealer and player respectively
@@ -102,13 +163,27 @@ def show_dealer_cards():
 
 # checks player total
 def player_total():
+    global player_points
     player_points = 0
     for card in player:
-        if isinstance(card['value'], list):
-            player_points += handle_ace_choice(card)
+        if isinstance(card['value'], list):  # Ace
+            player_points += handle_ace_choice(card, player_points)
         else:
             player_points += card['value']
     return player_points
+
+
+def dealer_total():
+    global dealer_points
+    dealer_points = 0
+    for card in dealer:
+        if isinstance(card['value'], list):
+            ace_value = 11 if dealer_points + 11 <= 21 else 1
+            dealer_points += ace_value
+        else:
+            dealer_points += card['value']
+    return dealer_points
+
 
 
 def check_player_bust(player_points):
@@ -121,24 +196,37 @@ def check_player_bust(player_points):
 # asks if the player wants to hit or stand and changes the draw variable accordingly
 def hit_or_stand():
     global draw
-    hit_stand = input("Hit or Stand? (hit/stand):")
-    if hit_stand.lower() == "hit":
-        draw = 1
-    elif hit_stand.lower() == "stand":
+    hit_stand = input("Hit or Stand? (hit/stand): ")
+    if hit_stand == "hit":
+        draw_player_card()
+        draw_dealer_card()
+        show_player_cards()
+        check_player_bust(player_points)
+        
+    elif hit_stand == "stand":
         draw = 2
-    return draw
 
 
 # gives the player a choice of using 11 or 1 for the value of the ace unless an 11 would make the player bust in which case ace is worth 1 
-def handle_ace_choice(card):
+def handle_ace_choice(card, current_points):
+    if 'final_value' in card:
+        return card['final_value']
+
     if card['card'].startswith('A'):
-        if player_total() + 11 <= 21:  
-            print(f"You drew an Ace! You can choose 11 or 1. Current total: {player_total()}")
-            ace_value = 11  
+        print(f"You drew an Ace! Current total: {current_points}")
+        if current_points + 11 <= 21:
+            ace_value = int(input("Would you like the Ace to be 11 or 1? (Enter 11 or 1): "))
+            while ace_value not in [1, 11]:
+                ace_value = int(input("Invalid choice. Please enter 11 or 1: "))
         else:
-            ace_value = 1  
+            print("An 11 would cause a bust, so the Ace will be worth 1.")
+            ace_value = 1
+
+
+        card['final_value'] = ace_value
         return ace_value
-    return 0  
+
+    return card['value']
 
 
 def win():
@@ -146,10 +234,24 @@ def win():
     winings = int(bet_amount * 1.5)
     cash = cash + winings
     cash = str(cash)
-    print(cash)
     with open('money.txt', 'w') as file:
         file.write(cash)
+
+#checks to see if a player won, tied, or lost after they stand
+def check_win(player_points, dealer_points):
+    global cash
+    if player_points > dealer_points or dealer_points > 21:
+        win()
+        print(f"You win! New balance: {cash}")
+    elif player_points == dealer_points:
+        print("It's a tie! Your bet is returned.")
+        with open('money.txt', 'r+') as file:
+            file.seek(0)
+            file.write(str(cash + bet_amount))
+    else:
+        print("Sorry, you lose.")
+
+
  
 
-bet()
-win()
+main()
